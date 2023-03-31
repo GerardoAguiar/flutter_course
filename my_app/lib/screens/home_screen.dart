9a1ae5c484c33a1180b2cc48a8ffb2d68cf70436
GerardoAguiar <79877702+GerardoAguiar@users.noreply.github.com>
2023-03-31 14:06:41 -0600

    StatelessWidget

--- my_app/lib/screens/home_screen.dart
+++ my_app/lib/screens/home_screen.dart
@@ -1,19 +1,41 @@
 import 'package:flutter/material.dart';
 
 class HomeScreen extends StatelessWidget{
+
   const HomeScreen({super.key});
+
+  //Variable, properties
 
   @override
   Widget build(BuildContext context) {
+    
+    const fontSize30 = TextStyle(fontSize: 30);
+    const fontSize20 = TextStyle(fontSize: 20);
+    int counter = 10;
+    
     return Scaffold(
       appBar: AppBar(
         centerTitle: true,
-        title: Text('Home Screen'),
+        title: const Text('Home Screen'),
         elevation: 0,
       ),
-      body: const Center(
-              child: Text('Home Screen'),
-            ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text('Counter', style: fontSize30), 
+            Text("$counter", style: fontSize20,)],
+        ),
+      ),
+      //floatingActionButtonLocation: FloatingActionButtonLocation.miniEndFloat,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          counter ++;
+          print("Counter $counter");
+        },
+        backgroundColor: const Color.fromARGB(255, 109, 129, 196),
+        child: const Icon(Icons.add),
+      ),
     );
   }
 }
